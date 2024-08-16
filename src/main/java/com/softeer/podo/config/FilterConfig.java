911e56a36376c67028c3dccd05388cfc0cdc1697
eckrin <bsh51515@naver.com>
2024-08-17 01:17:09 +0900

    feat: AOP 로깅을 위한 필터 추가
    - admin 기능에만 추가 예정

--- src/main/java/com/softeer/podo/config/FilterConfig.java
+++ src/main/java/com/softeer/podo/config/FilterConfig.java
@@ -1,6 +1,7 @@
 package com.softeer.podo.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.softeer.podo.log.filter.RequestCollectionFilter;
 import com.softeer.podo.security.jwt.ExceptionHandleFilter;
 import com.softeer.podo.security.jwt.JwtAuthenticationFilter;
 import com.softeer.podo.security.jwt.TokenProvider;
@@ -31,8 +32,18 @@ public class FilterConfig {
         FilterRegistrationBean<ExceptionHandleFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new ExceptionHandleFilter(objectMapper));
         registrationBean.addUrlPatterns("/test/auth"); // 필터를 적용할 URL 패턴
+        registrationBean.addUrlPatterns("/admin/*"); // 필터를 적용할 URL 패턴
         registrationBean.addUrlPatterns("/v1/*"); // 필터를 적용할 URL 패턴
         registrationBean.setOrder(1); // 필터의 순서 (숫자가 낮을수록 먼저 실행됨)
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<RequestCollectionFilter> requestCollectionFilter() {
+        FilterRegistrationBean<RequestCollectionFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RequestCollectionFilter());
+        registrationBean.addUrlPatterns("/admin/*"); // 필터를 적용할 URL 패턴
+        registrationBean.setOrder(3); // 필터의 순서 (숫자가 낮을수록 먼저 실행됨)
         return registrationBean;
     }
 }
