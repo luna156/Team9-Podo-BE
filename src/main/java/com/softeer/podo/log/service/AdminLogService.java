f8d3f741a3d7d3009c2ff016e4177026f39611b0
eckrin <bsh51515@naver.com>
2024-08-20 10:18:06 +0900

    feat: admin 로깅 api 추가
    - 간단한 페이징이라 QueryDsl 미사용
    - pagesize = 10

--- src/main/java/com/softeer/podo/log/service/AdminLogService.java
+++ src/main/java/com/softeer/podo/log/service/AdminLogService.java
@@ -1,10 +1,16 @@
 package com.softeer.podo.log.service;
 
+import com.softeer.podo.admin.model.dto.response.GetAdminLogListResponseDto;
+import com.softeer.podo.log.mapper.AdminLogMapper;
 import com.softeer.podo.log.model.dto.RequestInfoDto;
 import com.softeer.podo.log.model.entity.AdminLog;
 import com.softeer.podo.log.repository.AdminLogRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +21,12 @@ public class AdminLogService {
 
     private final AdminLogRepository adminLogRepository;
 
+    private final int PAGE_SIZE = 10;
+
+    /**
+     * Admin의 특정 동작을 rdb에 저장
+     * @param dto 요청 uri, 헤더, body 정보
+     */
     @Transactional
     public void createAdminLog(
             RequestInfoDto dto
@@ -26,5 +38,15 @@ public class AdminLogService {
                         .requestBody(dto.getRequestBody())
                         .build()
         );
+    }
+
+    /**
+     * Admin 로그 전체 불러오기
+     */
+    @Transactional(readOnly = true)
+    public GetAdminLogListResponseDto getAdminLogs(int pageNo) {
+        Pageable pageable = PageRequest.of(pageNo, PAGE_SIZE, Sort.by(Sort.Direction.DESC, "id"));
+        Page<AdminLog> logs = adminLogRepository.findAll(pageable);
+        return AdminLogMapper.AdminLogPageToAdminLogDtoList(logs);
     }
 }
