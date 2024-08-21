27e9817a3d2a58d1cdee00a46af88f413c3f2fd2
luna156 <v010309@naver.com>
2024-08-21 17:33:47 +0900

    fix: 랜덤 추첨 응모시 중복 응모하더라도 공유 링크 및 응모 여부를 반환하도록 수정

--- src/main/java/com/softeer/podo/event/repository/LotsShareLinkRepository.java
+++ src/main/java/com/softeer/podo/event/repository/LotsShareLinkRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface LotsShareLinkRepository extends JpaRepository<LotsShareLink, Long> {
+    boolean existsByLotsUser(LotsUser user);
     Optional<LotsShareLink> findByLotsUser(LotsUser user);
 }
