c910b506704a061da08e5f6f09092e47e7f19e91
luna156 <v010309@naver.com>
2024-08-22 12:29:09 +0900

    fix: cors 에러 해결

--- src/main/java/com/softeer/podo/PodoApplication.java
+++ src/main/java/com/softeer/podo/PodoApplication.java
@@ -2,9 +2,11 @@ package com.softeer.podo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
 @EnableRedisRepositories
@@ -15,4 +17,16 @@ public class PodoApplication {
 		SpringApplication.run(PodoApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:8000", "https://www.hyundaiseltos.site")  // 허용할 도메인
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메소드
+						.allowedHeaders("*"); // 허용할 헤더
+			}
+		};
+	}
 }
